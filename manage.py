aca25bac0696936eb0477bd71549e529af1d0c03
jazahn <jcleveng@fas.harvard.edu>
2013-09-05 06:16:57 -0400

    adding google app engine app config

--- manage.py
+++ manage.py
@@ -3,7 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "courseeval.settings")
+    
+    if os.environ.get("DJANGO_SETTINGS_MODULE"):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "courseeval.settings.googleappengine")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "courseeval.settings.sqlite")
+    
 
     from django.core.management import execute_from_command_line
 
