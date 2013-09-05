ed65f5174bde0ba2896d10042779f36b75ca0162
jazahn <jcleveng@fas.harvard.edu>
2013-09-05 03:56:16 -0400

    adding admin url

--- courseeval/urls.py
+++ courseeval/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+#from courseeval.admin import admin_site
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    #url(r'^admin/$', 'courseeval.core.index', name='coreIndex'),
     # url(r'^$', 'courseeval.views.home', name='home'),
     # url(r'^courseeval/', include('courseeval.foo.urls')),
 
@@ -13,5 +16,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
